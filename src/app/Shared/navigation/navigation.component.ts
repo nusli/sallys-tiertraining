4c9ade54abd84439f7bdae9a4c95713939ba63a4
Linus Brennecke <li.brennecke@web.de>
2025-10-05 14:08:12 +0200

    removed trash from previous dev

--- src/app/Shared/navigation/navigation.component.ts
+++ src/app/Shared/navigation/navigation.component.ts
@@ -8,8 +8,6 @@ import { Observable, Subscription, fromEvent } from 'rxjs';
 })
 export class NavigationComponent implements OnInit, OnDestroy {
   @Input() mobilePortrait = false;
-  navBackgroundHeight = 18.28;
-  navBackgroundMaxHeight = 165;
 
   // navbar control
   mobileNavOpen = false;
@@ -37,10 +35,8 @@ export class NavigationComponent implements OnInit, OnDestroy {
 
 
   ngOnInit(): void {
-    this.changeNavBackgroundHeight()
     this.resizeObservable$ = fromEvent(window, 'resize')
     this.resizeSubscription$ = this.resizeObservable$.subscribe( evt => {
-      this.changeNavBackgroundHeight();
     })
   }
 
@@ -94,57 +90,9 @@ export class NavigationComponent implements OnInit, OnDestroy {
       this.stable_open = false;
       this.personal_open = false;
     }
-    this.changeNavBackgroundHeight();
   }
 
-  changeNavBackgroundHeight(): void
-  {
-    if(!this.mobilePortrait){
-      this.navBackgroundHeight = 9.2;
-      this.navBackgroundMaxHeight = 165;
-      if(this.windowRatio >= 1.2){
-        this.navBackgroundHeight = 11;
-      }
-      if(this.windowRatio >= 1.4){
-        this.navBackgroundHeight = 12;
-      }
-      if(this.windowRatio >= 1.6){
-        this.navBackgroundHeight = 15;
-      }
-      if(this.windowRatio >= 2){
-        this.navBackgroundHeight = 22;
-        this.navBackgroundMaxHeight = 210;
-      }
-      if(this.windowRatio >= 2.6){
-        this.navBackgroundHeight = 29;
-        this.navBackgroundMaxHeight = 250;
-      }
-      if(this.windowRatio >= 3.6){
-        this.navBackgroundHeight = 40;
-        this.navBackgroundMaxHeight = 230;
-      }
-    }else{
-      this.navBackgroundHeight = 14.5;
-      if(this.windowRatio <= 0.65){
-        this.navBackgroundHeight = 12;
-       }
-      if(this.windowRatio <= 0.5){
-       this.navBackgroundHeight = 10;
-      }
-      if(this.windowRatio <= 0.43){
-        this.navBackgroundHeight = 14.5;
-       }
-      if(this.mobileNavOpen){
-        this.navBackgroundHeight = 29;
-        this.navBackgroundMaxHeight = 230;
-        if(this.groups_open || this.individual_open || this.horse_open || this.stable_open || this.personal_open){
-          this.navBackgroundHeight = 44;
-          this.navBackgroundMaxHeight = 340;
-        }
-      }
-
-    }
-  }
+  
   ngOnDestroy(): void {
     if(this.resizeSubscription$ != null){
       this.resizeSubscription$.unsubscribe()
