ce81ba73959e764a16115874104166d6eee99faf
Linus Brennecke <li.brennecke@web.de>
2025-10-26 13:01:21 +0100

    added gallery component + stable gallery

--- src/app/Shared/image-gallery/image-gallery.component.ts
+++ src/app/Shared/image-gallery/image-gallery.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, HostListener, Input } from '@angular/core';
 
 @Component({
   selector: 'app-image-gallery',
@@ -8,6 +8,83 @@ import { Component, Input } from '@angular/core';
 export class ImageGalleryComponent {
 
   @Input() images!: {url: string, alt: string}[];
+
+  showBigImage = false;
+  selectedImage = 0;
+
+  @HostListener('document:keydown.escape', ['$event'])
+  onEscapeKey(event: KeyboardEvent) {
+    // User can always close the big image view by pressing escape
+    this.showBigImage = false;
+  }
+
+  private touchStartX: number = 0;
+  private touchEndX: number = 0;
+  private swipeStartTime: number = 0;
+  private swipeEndTime: number = 0;
+
+  @HostListener('touchstart', ['$event'])
+  onTouchStart(event: TouchEvent) {
+    this.touchStartX = event.changedTouches[0].screenX;
+    this.swipeStartTime = new Date().getTime();
+  }
+
+  @HostListener('touchend', ['$event'])
+  onTouchEnd(event: TouchEvent) {
+    this.touchEndX = event.changedTouches[0].screenX;
+    this.swipeEndTime = new Date().getTime();
+    this.handleSwipe();
+  }
+
+  private handleSwipe() {
+    const swipeThreshold = 50; // minimum distance for a swipe
+    const swipeDurationThreshold = 1000;
+    const swipeDistance = this.touchEndX - this.touchStartX;
+
+    if (Math.abs(swipeDistance) > swipeThreshold && this.swipeEndTime - this.swipeStartTime < swipeDurationThreshold ) {
+      if (swipeDistance > 0) {
+        this.onSwipeRight();
+      } else {
+        this.onSwipeLeft();
+      }
+    }
+  }
+
+  onSwipeLeft() {
+    this.showNextImage()
+    // Your function here
+  }
+
+  onSwipeRight() {
+    this.showPreviousImage()
+    // Your function here
+  }
+
+  openBigImageView(index: number) {
+    this.showBigImage = true;
+    this.selectedImage = index;
+  }
+
+  showPreviousImage(event?: Event) {
+    if (this.selectedImage == 0) this.selectedImage = this.images.length-1
+    else this.selectedImage--;
+    // parent element closes the view on click
+    // so we prevent the propagation of the click to that element
+    if (event) {
+      event.stopPropagation();
+    }
+    
+  }
+
+  showNextImage(event?: Event) {
+    if (this.selectedImage == this.images.length-1) this.selectedImage = 0
+    else this.selectedImage++;
+    // parent element closes the view on click
+    // so we prevent the propagation of the click to that element
+    if (event) {
+      event.stopPropagation();
+    }
+  }
 
   constructor() {
     
